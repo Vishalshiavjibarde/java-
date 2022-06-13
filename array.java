11cffcb0222a7b4e8af276ee0d5ba8e2cceab652
Barde Vishal Shivaji <95370589+Vishalshiavjibarde@users.noreply.github.com>
2022-06-13 14:23:54 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,16 @@
+// array
+import java.io.*;
+
+public class array
+{
+
+    public static void main(String[]args) throws IOException
+    {
+
+        int a[]={10,20,30,40,50};
+        for(int i=0; i<10;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
